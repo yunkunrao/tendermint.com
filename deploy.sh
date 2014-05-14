8cca132aae05a959083e4a239aeb34b99fb9c1b1
Jae Kwon <jkwon.work@gmail.com>
2014-05-14 16:48:19 -0700

    rebuilding site Wed May 14 16:48:19 PDT 2014

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:tendermint/tendermint.com.git gh-pages
+git push origin sources
+git subtree push --prefix=public git@github.com:tendermint/tendermint.github.io.git master
