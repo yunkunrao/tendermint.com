cc8a0afd4e226ed802816e2fd571c5fc00889047
Jae Kwon <jkwon.work@gmail.com>
2014-06-13 14:36:51 -0700

    rebuilding site Fri Jun 13 14:36:51 PDT 2014

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,21 @@
 
 echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
-# Build the project.
+### SOURCES
+
 hugo
-
-# Add changes to git.
 git add -A
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin sources
+
+
+### PUBLIC (master)
+
 cd public
+git add -A
+git commit -m "$msg"
 git push origin master
