ec8c05e28c9129dded33fd6dd90eae3e106682dc
Peng Zhong <peng@nylira.com>
2016-11-02 15:38:05 +0800

    remove doubled toc on Docs

--- src/components/pages/Docs/Master.vue
+++ src/components/pages/Docs/Master.vue
@@ -18,24 +18,6 @@
       <router-link :to="'/docs/internals/configuration'">Configuration</router-link>
       <router-link :to="'/docs/internals/light-client-protocol'">Light Client Protocol</router-link>
       <router-link class="block" :to="'/docs/commands'">Tendermint Commands</router-link>
-
-      <div class="title">Docs</div>
-      <router-link :to="'/docs'" exact>Index</router-link>
-      <router-link :to="'/docs/faq'">FAQ</router-link>
-      <router-link :to="'/docs/roadmap'">Roadmap</router-link>
-      <div class="title">Guides</div>
-      <router-link :to="'/docs/guides/app-development'">App Development</router-link>
-      <router-link :to="'/docs/guides/app-architecture'">App Architecture</router-link>
-      <div class="title">Tendermint Internals</div>
-      <router-link :to="'/docs/internals/introduction'">Introduction</router-link>
-      <router-link :to="'/docs/internals/validators'">Validators</router-link>
-      <router-link :to="'/docs/internals/consensus'">Byzantine Consensus Algorithm</router-link>
-      <router-link :to="'/docs/internals/block-structure'">Block Structure</router-link>
-      <router-link :to="'/docs/internals/rpc'">RPC</router-link>
-      <router-link :to="'/docs/internals/genesis'">Genesis</router-link>
-      <router-link :to="'/docs/internals/configuration'">Configuration</router-link>
-      <router-link :to="'/docs/internals/light-client-protocol'">Light Client Protocol</router-link>
-      <router-link class="block" :to="'/docs/commands'">Tendermint Commands</router-link>
     </nav>
     <thumb-button v-show="isActive" icon="close"></thumb-button>
     <thumb-button v-show="!isActive" icon="bars"></thumb-button>
