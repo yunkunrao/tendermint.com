a4bf71c2d9cb21d077b6493f045480882afccc2a
Ethan Buchman <ethan@coinculture.info>
2016-11-14 20:26:39 -0500

    home: drop It's

--- src/components/sections/HomeBenefits.vue
+++ src/components/sections/HomeBenefits.vue
@@ -5,42 +5,42 @@
       <div class="block">
         <i class="fa fa-server"></i> 
         <div class="text">
-          <div class="key">It's byzantine fault-tolerant.</div>
+          <div class="key">Byzantine Fault-Tolerant</div>
           <div class="value">Tendermint tolerates up to a 1/3 of your machines failing arbitrarily. This includes explicitly malicious behaviour.</div>
         </div>
       </div>
       <div class="block">
         <i class="fa fa-clone"></i>
         <div class="text">
-          <div class="key">It's got state machine replication.</div>
-          <div class="value">TMSP lets you replicate deterministic state machines, which can be written in any programming language.</div>
+          <div class="key">State Machine Replication</div>
+          <div class="value">Tendermint can replicate deterministic state machines written in any programming language.</div>
         </div>
       </div>
       <div class="block">
         <i class="fa fa-lock"></i>
         <div class="text">
-          <div class="key">It's got secure P2P.</div>
+          <div class="key">Secure P2P</div>
           <div class="value">Gossip protocols and peer discovery are secured via Tendermint's authenticated encryption system.</div>
         </div>
       </div>
       <div class="block">
         <i class="fa fa-fighter-jet"></i>
         <div class="text">
-          <div class="key">It's lighting fast.</div>
+          <div class="key">Lighting fast</div>
           <div class="value">Tendermint supports thousands of transaction per second at 1000ms latencies.</div>
         </div>
       </div>
       <div class="block">
         <i class="fa fa-cloud-upload"></i>
         <div class="text">
-          <div class="key">It's easy to deploy.</div>
-          <div class="value">Use mintnet to deploy your app to any cloud provider with docker-machine support.</div>
+          <div class="key">Easy to deploy</div>
+          <div class="value">Use mintnet to deploy your app to any cloud provider supported by docker-machine.</div>
         </div>
       </div>
       <div class="block">
         <i class="fa fa-code"></i>
         <div class="text">
-          <div class="key">It's 100% open source.</div>
+          <div class="key">100% open source</div>
           <div class="value">All of Tendermint's source code is licensed under Apache 2.0. Check it out on <a  href="https://github.com/tendermint">GitHub</a>.</div>
         </div>
       </div>
