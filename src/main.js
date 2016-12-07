6814aa9eb8c4ec64f498b5ea2426399ae57fa085
Peng Zhong <peng@nylira.com>
2016-12-07 12:19:30 +0800

    optimizations

--- src/main.js
+++ src/main.js
@@ -3,18 +3,25 @@
 import Vue from 'vue'
 import VueHead from 'vue-head'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-ua'
 
 import App from './App'
 
 // sync store and router
 import { sync } from 'vuex-router-sync'
-import store from './store/index.js' // vuex store instance
+import store from './store/index.js'
 import router from './router/index.js'
-
-sync(store, router) // done.
+sync(store, router)
 
 Vue.use(VueHead)
 Vue.use(VueRouter)
+Vue.use(VueAnalytics, {
+  appName: 'Tendermint Website',
+  appVersion: '1.0',
+  trackingId: 'UA-51029217-1',
+  debug: false,
+  vueRouter: router
+})
 
 /* eslint no-unused-vars: 0 */
 const app = new Vue({
