4f3462e23dac8ef14d4da3dc4b1c22c60c450705
Peng Zhong <peng@nylira.com>
2017-01-20 10:19:49 +0900

    update intro link

--- src/content/docs/index.vue
+++ src/content/docs/index.vue
@@ -1,8 +1,8 @@
 <template><div><h1>Documentation</h1>
-<p>Start at the <router-link to=/docs/internals/merkle>Tendermint Internals Merkle</router-link> if you&#x2019;re new.</p>
+<p>If you&#x2019;re new here, start with the <router-link to=/intro>Tendermint Intro</router-link>.</p>
 <p>Otherwise, find more goodies on this page!</p>
 <h2>How-To Guides</h2>
 <p>Get started building your ABCI application or server by reading the <router-link to=/docs/guides/app-development>App Development guide</router-link>.</p>
 <p>To learn about integrating with clients, see the <router-link to=/docs/guides/app-architecture>App Architecture</router-link> guide.</p>
-<p>See Tendermint&#x2019;s <router-link to=/ecosystem>Software Ecosystem</router-link>for motivation and inspiration!</p>
+<p>See Tendermint&#x2019;s <router-link to=/ecosystem>Software Ecosystem</router-link> for motivation and inspiration!</p>
 </div></template>
