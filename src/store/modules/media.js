422228708b96e8037306678ab2576a9239e5a704
Peng Zhong <peng@nylira.com>
2017-03-07 14:23:27 +0800

    update news

--- src/store/modules/media.js
+++ src/store/modules/media.js
@@ -1,6 +1,24 @@
 const state = {
   all: [
     {
+      title: 'JP Morgan, Santander Said to Join New Ethereum Blockchain Group',
+      date: '2017-02-13',
+      company: 'CoinDesk',
+      url: 'http://www.coindesk.com/jp-morgan-santander-said-join-enterprise-ethereum-blockchain-group/'
+    },
+    {
+      title: 'Tendermint talks about consensus and the \'internet of blockchains\'',
+      date: '2016-12-07',
+      company: 'International Business Times UK',
+      url: 'http://www.ibtimes.co.uk/tendermint-talks-about-consensus-internet-blockchains-1589640'
+    },
+    {
+      title: 'Tendermint Wins Innovation Award and Announces Cosmos at International Blockchain Week',
+      date: '2016-09-23',
+      company: 'Bitcoin Magazine',
+      url: 'https://bitcoinmagazine.com/articles/tendermint-wins-innovation-award-and-announces-cosmos-at-international-blockchain-week-1474657507'
+    },
+    {
       title: 'Tendermint Added to Microsoft\'s Azure BaaS Platform',
       date: '2016-03-18',
       company: 'CoinTelegraph',
@@ -23,12 +41,6 @@ const state = {
       date: '2016-02-06',
       company: 'CoinTelegraph',
       url: 'http://cointelegraph.com/news/tendermints-mintnet-for-launching-blockchains-to-any-cloud-provider'
-    },
-    {
-      title: 'Tendermint Wins Innovation Award and Announces Cosmos at International Blockchain Week',
-      date: '2016-09-23',
-      company: 'Bitcoin Magazine',
-      url: 'https://bitcoinmagazine.com/articles/tendermint-wins-innovation-award-and-announces-cosmos-at-international-blockchain-week-1474657507'
     }
   ]
 }
