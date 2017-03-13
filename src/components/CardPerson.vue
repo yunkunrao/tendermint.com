21dfe32663517958d1dd343b8aebb9e72636f3e1
Peng Zhong <peng@nylira.com>
2017-03-13 15:09:55 +0800

    about page improvement

--- src/components/CardPerson.vue
+++ src/components/CardPerson.vue
@@ -1,43 +1,42 @@
 <template>
 <div class="card-person">
-  <div class="avatar-container">
+  <div class="card-person-header">
     <div class="avatar">
       <img v-if="person.slug !==''" :src="avatarSrc(person.slug)" :alt="person.name">
       <i v-else class="fa fa-user"></i>
     </div>
-  </div>
-  <div class="data">
-    <div class="name">{{ person.name }}</div>
-    <div class="subtitle">{{ person.subtitle }}</div>
-    <div class="title">{{ person.title }}</div>
-    <div class="bio" v-if="person.bio">{{ person.bio }}</div>
-    <div class="ids">
-      <a v-if="person.ids.email"
-        :href="'mailto:' + person.ids.email + '@tendermint.com'">
-        <i class="fa fa-envelope"></i>
-        <span class="label">{{ person.ids.email }}@tendermint.com</span>
-      </a>
-      <a v-if="person.ids.github" :href="'https://github.com/' + person.ids.github">
-        <i class="fa fa-github"></i>
-        <span class="label">github.com/{{ person.ids.github }}</span>
-      </a>
-      <a v-if="person.ids.keybase" :href="'https://keybase.io/' + person.ids.keybase">
-        <i class="fa fa-key"></i>
-        <span class="label">keybase.io/{{ person.ids.keybase }}</span>
-      </a>
-      <a v-if="person.ids.linkedin" :href="'https://www.linkedin.com/in/' + person.ids.linkedin">
-        <i class="fa fa-linkedout"></i>
-        <span class="label">{{ person.name }}</span>
-      </a>
-      <a v-if="person.ids.twitter" :href="'https://twitter.com/' + person.ids.twitter">
-        <i class="fa fa-twatter"></i>
-        <span class="label">@{{ person.ids.twitter }}</span>
-      </a>
-      <a v-if="person.ids.website" :href="'http://' + person.ids.website">
-        <i class="fa fa-globe"></i>
-        <span class="label">{{ person.ids.website }}</span>
-      </a>
+    <div class="text">
+      <div class="name">{{ person.name }}</div>
+      <div class="title">{{ person.title }}</div>
     </div>
+  </div>
+  <div class="card-person-main" v-if="person.bio">{{ person.bio }}</div>
+  <div class="card-person-footer">
+    <a v-if="person.ids.email"
+      :href="'mailto:' + person.ids.email + '@tendermint.com'">
+      <i class="fa fa-envelope"></i>
+      <span class="label">{{ person.ids.email }}@tendermint.com</span>
+    </a>
+    <a v-if="person.ids.github" :href="'https://github.com/' + person.ids.github">
+      <i class="fa fa-github-alt"></i>
+      <span class="label">github.com/{{ person.ids.github }}</span>
+    </a>
+    <a v-if="person.ids.keybase" :href="'https://keybase.io/' + person.ids.keybase">
+      <i class="fa fa-key"></i>
+      <span class="label">keybase.io/{{ person.ids.keybase }}</span>
+    </a>
+    <a v-if="person.ids.linkedin" :href="'https://www.linkedin.com/in/' + person.ids.linkedin">
+      <i class="fa fa-linkedout"></i>
+      <span class="label">{{ person.name }}</span>
+    </a>
+    <a v-if="person.ids.twitter" :href="'https://twitter.com/' + person.ids.twitter">
+      <i class="fa fa-twatter"></i>
+      <span class="label">@{{ person.ids.twitter }}</span>
+    </a>
+    <a v-if="person.ids.website" :href="'http://' + person.ids.website">
+      <i class="fa fa-globe"></i>
+      <span class="label">{{ person.ids.website }}</span>
+    </a>
   </div>
 </div>
 </template>
@@ -58,32 +57,21 @@ export default {
 @require '../styles/variables.styl'
 
 .card-person
-  margin 0 auto 2rem
+  border 1px solid bc
+  margin 0 0 1rem
 
-  display flex
-  flex-flow column nowrap
-  align-items justify
-
-  padding 0 x
-  max-width 60*x
   &:last-of-type
     margin-bottom 0
 
-  .avatar-container, .title
-    margin-bottom x
-
-  .bio
-    margin-bottom 1.5*x
-
-  .avatar-container
-    display flex
-    justify-content center
+.card-person-header
+  display flex
+  border-bottom 1px solid bc
+  padding 1rem
 
   .avatar
-    border 1px solid bc
-    padding 0.25rem
-    height 10rem
-    width 10rem
+    height 3rem
+    width 3rem
+    margin-right 0.75rem
 
     display flex
 
@@ -94,66 +82,79 @@ export default {
       display block
 
     i.fa
-      font-size 4rem
-      color link
+      font-size 2rem
+      color light
       background bc
       display flex
       align-items center
       justify-content center
-      color link
 
-  .name, .title
-    text-align center
+  .text
+    display flex
+    flex-flow column nowrap
+    justify-content center
 
-  .name
-    font-weight bold
+    .name
+      font-weight bold
 
-  .title
-    text-transform uppercase
-    font-size 0.75em
-    font-weight 500
-    line-height 2
-    color light
+    .title
+      color dim
+      font-size 0.875rem
 
-  .ids
-    a
-      display block
-      border-top 1px solid bc
-      padding 0.25*x
-      width 100%
+.card-person-main
+  font-size 0.75rem
+  border-bottom 1px solid bc
+  padding 0.75rem 1rem
+
+.card-person-footer
+  display flex
+  flex-flow row wrap
+  background lighten(bc, 75%)
+  a
+    width 3rem
+    height 2rem
+    display block
+    display flex
+    align-items center
+    justify-content center
+    border-right 1px dotted bc
+    &:last-of-type
+      border-right none
+    i.fa
+      color light
+      font-size 1rem
+    &:hover
       i.fa
-        display inline-block
-        width 1.75*x
-        text-align center
-      &:hover .label
-        text-decoration underline
+        color link
+    .label
+      display none
 
-@media screen and (min-width: 400px)
+@media screen and (min-width:414px)
+  .card-person-main
+    font-size 0.875rem
+    
+@media screen and (min-width:1280px)
   .card-person
-    .data
+    display flex
+    flex-flow row wrap
+
+  .card-person-header
+    flex 1
+    .avatar
+      height 5rem
+      width 5rem
+      margin-right 1rem
+    .text
+      height 5rem
       .name
-        font-size 1.125*x
+        font-size 1.25rem
+      .title
+        font-size 1rem
 
-@media screen and (min-width: 720px)
-  .card-person
-    flex-flow row
-    margin-bottom 3*x
-
-    .avatar-container
-      margin-bottom 0
-
-    .data
-      flex 1
-      padding-left 2*x
-
-      .name, .title
-        text-align left
-
-      .name
-        font-size 1.25*x
-@media screen and (min-width: 960px)
-  .card-person
-    .data
-      display flex
-      flex-flow column nowrap
+  .card-person-main
+    flex 2
+    font-size 1rem
+    padding 1rem
+  .card-person-footer
+    flex 0 0 100%
 </style>
