a907a41c0f6534df3a596387ba5285777d1089ac
Peng Zhong <peng@nylira.com>
2017-03-14 21:28:42 +0800

    fix about page

--- src/store/modules/people.js
+++ src/store/modules/people.js
@@ -167,7 +167,7 @@ const state = {
       }
     },
     {
-      slug: 'franck-mikulecz',
+      slug: '',
       name: 'Franck Mikulecz',
       title: 'Advisor',
       bio: 'Franck is founder and CEO of BAXTER, a group of companies focused on foreign exchange and technology. Having spent years in a market perpetually dominated by Banks, Franck feels the urge to help deliver the "disruption" promise of the Blockchain. With FXCH Ltd, a Clearing and Settlement startup, his goal is simply to drag FX, screaming and kicking into the cryptocurrency era.',
