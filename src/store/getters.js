c7abc2690c7e8bebcfb598638e94491cc85da740
Peng Zhong <peng@nylira.com>
2017-04-06 13:15:59 +0800

    remove unused companies module

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,5 @@
 export const allBounties = state => state.bounties.all
 export const allCareers = state => state.careers.all
-export const allCompanies = state => state.companies.all
 export const allMedia = state => state.media.all
 export const allPeople = state => state.people.all
 export const allPosts = state => state.posts.all
