21ffabe35d4b3a314d7309c6dab17d7e47e84db7
Peng Zhong <peng@nylira.com>
2017-07-14 10:58:01 +0800

    redirect broken links

--- src/router/routesRedirects.js
+++ src/router/routesRedirects.js
@@ -1,21 +1,33 @@
 export default [
-  { path: '/intro/tmsp-overview', redirect: '/intro/abci-overview' },
-  { path: '/intro/tendermint-vs', redirect: '/intro/tendermint-vs-x' },
+  // getting started
+  { path: '/intro/getting-started/first-app', redirect: '/docs/getting-started/first-app' },
+  { path: '/intro/getting-started/deploy-testnet', redirect: '/docs/getting-started/deploy-testnet' },
+  { path: '/intro/getting-started/next-steps', redirect: '/docs/getting-started/next-steps' },
   { path: '/intro/first-tmsp',
     redirect: '/docs/getting-started/first-app' },
   { path: '/docs/getting-started/first-abci',
     redirect: '/docs/getting-started/first-app' },
   { path: '/docs/getting-started/first-abci-app',
     redirect: '/docs/getting-started/first-app' },
+
+  // renaming
+  { path: '/intro/tmsp-overview', redirect: '/intro/abci-overview' },
+  { path: '/intro/tendermint-vs', redirect: '/intro/tendermint-vs-x' },
+
+  // download
+  { path: '/downloads', redirect: '/download' },
   { path: '/docs/getting-started/install', redirect: '/download' },
   { path: '/docs/getting-started/download', redirect: '/download' },
   { path: '/intro/download-tendermint', redirect: '/download' },
   { path: '/docs/getting-started/download-tendermint', redirect: '/download' },
+
   { path: '/docs/specs/consensus',
     redirect: '/docs/specs/byzantine-consensus-algorithm' },
   { path: '/blog/tendermint-socket-protocol',
     redirect: '/blog/abci-the-application-blockchain-interface' },
   { path: '/docs/guides/install', redirect: '/docs/guides/install-from-source' },
+
+  // other pages
   { path: '/code', redirect: '/docs' },
   { path: '/guide', redirect: '/docs' },
   { path: '/jobs', redirect: '/careers' },
