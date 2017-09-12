9dcc5fc7da284aae5e7884e5b1b5aa0f6842f6ab
Peng Zhong <peng@nylira.com>
2017-09-12 13:06:49 +0800

    remove posts and fix build issues

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,7 +2,6 @@ export const allBounties = state => state.bounties.all
 export const allCareers = state => state.careers.all
 export const allMedia = state => state.media.all
 export const allPeople = state => state.people.all
-export const allPosts = state => state.posts.all
 export const allPresentations = state => state.presentations.all
 export const allSoftware = state => state.software
 export const config = state => state.config
--- src/store/modules/posts.js
+++
@@ -1,9 +0,0 @@
-import data from '../json/posts.json'
-
-const state = {
-  all: data
-}
-
-export default {
-  state
-}
