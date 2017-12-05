a90b4a21bb33e7668998100ee98a8d06f775b32f
Peng Zhong <peng@nylira.com>
2017-12-05 11:04:33 +0900

    update text, remove unused code

--- src/components/PageBountiesIndex.vue
+++ src/components/PageBountiesIndex.vue
@@ -9,28 +9,29 @@
     </page-header>
     <article-body>
       <h2>Bug Bounty Program for Tendermint & Cosmos</h2>
-      <p>If you discover a vulnerability in Tendermint Core, the Tendermint libraries or the ABCI repository you might be eligible for our bounty program:</p>
+      <p>If you discover a vulnerability in Tendermint Core, the Tendermint libraries or the ABCI repository you might be eligible for our bounty program. The vulnerability must be applicable to Tendermint Core running the Persistent Dummy application in-process, or out-of-process via the socket server.</p>
 
+      <h3>Applicable repositories</h3>
+      <p>You can hunt for bugs and vulnerabilities in these following Tendermint repositories:</p>
       <ul>
-        <li><a href="https://github.com/tendermint/tendermint">tendermint</a></li>
-        <li><a href="https://github.com/tendermint/abci">abci</a></li>
-        <li><a href="https://github.com/tendermint/go-wire">go-wire</a></li>
-        <li><a href="https://github.com/tendermint/go-crypto">go-crypto</a></li>
-        <li><a href="https://github.com/tendermint/iavl">iavl</a></li>
+        <li><a target="_blank" href="https://github.com/tendermint/tendermint">tendermint/tendermint</a></li>
+        <li><a target="_blank" href="https://github.com/tendermint/abci">tendermint/abci</a></li>
+        <li><a target="_blank" href="https://github.com/tendermint/go-crypto">tendermint/go-crypto</a></li>
+        <li><a target="_blank" href="https://github.com/tendermint/go-wire">tendermint/go-wire</a></li>
+        <li><a target="_blank" href="https://github.com/tendermint/iavl">tendermint/iavl</a></li>
       </ul>
 
-      <p>The vulnerability must be applicable to Tendermint Core running the Persistent Dummy application in-process, or out-of-process via the socket server.</p>
-
       <h3>Reporting a vulnerability</h3>
-      <p> To responsibly report a vulnerability for points in the bug bounty program, please fill out <a target="_blank" href="https://goo.gl/forms/pAohzyeZTpszEZxm2">this form</a>. Our team will evaluate the report, rate its severity, and then get back to you as fast as we can.</p>
+      <p> To responsibly report a vulnerability for points in the bug bounty program, please fill out the form below. Our team will evaluate the report, rate its severity, and then get back to you as fast as we can.</p>
+      <btn href="https://goo.gl/forms/pAohzyeZTpszEZxm2" target="_blank" value="Report Vulernability" size="lg"></btn>
 
       <h3>Rewards</h3>
       <p> The following list shows the rewards for each level of severity. One point is redeemable for <code>$1.00 USD</code> with BTC or ETH.</p>
       <ul>
-        <li><strong>Critical</strong> - up to 7,500+ points</li>
-        <li><strong>High</strong> - up to 5,000 points</li>
-        <li><strong>Medium</strong> - up to 2,500 points</li>
-        <li><strong>Low</strong> - up to 1,000 points</li>
+        <li><strong>Critical Severity</strong> - up to 7,500+ points</li>
+        <li><strong>High Severity</strong> - up to 5,000 points</li>
+        <li><strong>Medium Severity</strong> - up to 2,500 points</li>
+        <li><strong>Low Severity</strong> - up to 1,000 points</li>
         <li><strong>Note</strong> - up to 500 points</li>
       </ul>
     </article-body>
@@ -38,10 +39,7 @@
 </template>
 
 <script>
-import $ from 'jquery'
-import { mapGetters } from 'vuex'
-import { union, orderBy } from 'lodash'
-import CardBounty from './CardBounty'
+import Btn from '@nylira/vue-button'
 import NiSection from './NiSection'
 import PageHeader from '@nylira/vue-page-header'
 import PageSplit from '@nylira/vue-page-split'
@@ -49,52 +47,14 @@ import ArticleBody from '@nylira/vue-article-body'
 export default {
   name: 'page-bounties-index',
   components: {
-    CardBounty,
+    Btn,
+    ArticleBody,
     NiSection,
     PageHeader,
-    PageSplit,
-    ArticleBody
-  },
-  computed: {
-    tags () {
-      let tags = []
-      this.bounties.map(function (bounty) {
-        tags = union(tags, bounty.tags)
-      })
-      return tags
-    },
-    filteredBounties () {
-      let activeTag = this.activeTag
-      let orderedBounties = orderBy(this.bounties, ['weight'], ['asc'])
-
-      if (activeTag === 'all') {
-        return orderedBounties
-      } else {
-        return orderedBounties.filter(bounty => bounty.tags.includes(activeTag))
-      }
-    },
-    ...mapGetters({ bounties: 'allBounties' })
-  },
-  data () {
-    return {
-      activeTag: 'all'
-    }
-  },
-  methods: {
-    setActiveTag ($event, tagName) {
-      $('.tag').removeClass('active')
-      if (tagName) {
-        $($event.target).addClass('active')
-        this.activeTag = tagName
-        return
-      }
-      $('#tag-all').addClass('active')
-      this.activeTag = 'all'
-      return
-    }
+    PageSplit
   },
   mounted () {
-    document.title = 'Bounties - Tendermint'
+    document.title = 'Bug Bounties - Tendermint'
   }
 }
 </script>
