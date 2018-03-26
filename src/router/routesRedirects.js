d99d992c646368827c3e74c99beb447eac25c283
Peng Zhong <peng@nylira.com>
2018-03-26 14:14:38 +0200

    fix careers issues

--- src/router/routesRedirects.js
+++ src/router/routesRedirects.js
@@ -4,22 +4,14 @@ export default [
   { path: '/posts/:entry', redirect: '/blog' },
   { path: '/blog/:entry', redirect: '/blog' },
 
-  // redirect old JSON Career urls to new Markdown Career urls
-  { path: '/careers/networking-consensus-engineer',
-    redirect: '/careers/developer-consensus' },
-  { path: '/careers/apps-developer', redirect: '/careers/developer-cosmos' },
-  { path: '/careers/devops-sysops-engineer', redirect: '/careers/developer-devops' },
-  { path: '/careers/ethereum-developer', redirect: '/careers/developer-ethereum' },
-  { path: '/careers/p2p-networking-engineer', redirect: '/careers/developer-p2p' },
-  { path: '/careers/ops-office-manager', redirect: '/careers/office-manager-sf' },
-  { path: '/careers/berlin-office-manager', redirect: '/careers/office-manager-berlin' },
-
   // other pages
+  { path: '/apply', redirect: '/careers' },
   { path: '/code', redirect: '/docs' },
   { path: '/download', redirect: '/downloads' },
   { path: '/guide', redirect: '/docs' },
   { path: '/jobs', redirect: '/careers' },
   { path: '/jobs/:entry', redirect: '/careers/:entry' },
+  { path: '/join', redirect: '/careers' },
   { path: '/media', redirect: '/presentations' },
   { path: '/media/:entry', redirect: '/presentations/:entry' },
   { path: '/guides/contributing', redirect: '/docs/guides/contributing' },
